3afe1087b9fa50a9655b2a9d22aff2e1b47431f8
simenmas <115172003+simenmas@users.noreply.github.com>
2024-10-14 10:04:20 +0200

    Added downloadfile
    La til funksjon som laster ned fil gitt dag, har ikke fiksa for årstall. Må legge til logg inn som beskrevet i readme

---
+++ backend/downloadfile.py
@@ -0,0 +1,29 @@
+import requests
+import gzip
+import os
+day = 287
+folder = 'backend/unzipped/'
+
+def lastned(day):
+    filename = f'BRDC00IGS_R_2024{day}0000_01D_MN.rnx.gz'
+
+    url = f'https://cddis.nasa.gov/archive/gnss/data/daily/2024/brdc/{filename}'
+
+    if not os.path.isfile(folder+filename[:-3]):
+        r = requests.get(url)
+        c = r.content
+
+        with open(filename, 'wb') as fd:
+            fd.write(c)
+
+        with open(filename,'rb') as fd:
+            gzip_fd = gzip.GzipFile(fileobj=fd)
+            gzip_fd = gzip_fd.read()
+        os.remove(filename)
+
+        with open(folder+filename[:-3],'wb') as f:
+            f.write(gzip_fd)
+    else:
+        print('File Exists')
+
+lastned(day)
